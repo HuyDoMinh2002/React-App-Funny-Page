e41a764886454cf60756ceca572d2dd903c01892
Do <do.huy@accenture.com>
2025-08-05 12:49:56 +0700

    second commit

---
+++ infra/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+# Provider Settings
+provider "google" {
+  project     = var.project_id
+  region      = var.gcp_project_region
+  zone        = var.gcp_project_zone           # Choose your zone
+  credentials = file(var.gcp_credentials_file) # Path to your service account key file
+}
+
+# Compute API (VPC, Subnet, Firewall rules, VMs)
+resource "google_project_service" "compute" {
+  project            = var.project_id
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+module "network" {
+  source             = "./modules/network"
+  project_id         = var.project_id
+  gcp_project_region = var.gcp_project_region
+}
---
+++ infra/variables.tf
@@ -0,0 +1,23 @@
+
+
+# GCP Variable
+variable "project_id" {
+  description = "The GCP project id"
+  type        = string
+}
+
+variable "gcp_project_region" {
+  description = "The GCP project region"
+  type        = string
+}
+
+variable "gcp_project_zone" {
+  description = "The GCP project zone"
+  type        = string
+}
+
+variable "gcp_credentials_file" {
+  description = "Path to the GCP service account key file"
+  type        = string
+  sensitive   = true
+}
